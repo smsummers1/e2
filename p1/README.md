78e0d4de39c790a8fdd2d6067e22db103baffd52
Shawna <shawnamariesummers@gmail.com>
2019-09-16 17:44:17 -0400

    1st commit to Project 1

---
+++ p1/README.md
@@ -0,0 +1,8 @@
+# Project 1
++ By: Shawna Summers
++ Production URL: <http://p1.shawnasummers.xyz>
+
+## Outside resources
+(https://historynewsnetwork.org/article/169795)
+
+## Notes for instructor
---
+++ p1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Author: Shawna Summers -->
+    
+<head>
+    <title>Project 1</title>
+    <meta charset="utf-8"/>
+</head>
+    
+<body>
+    <h1>Dr. Martin Luther King, Jr. & Mahatma Gandhi</h1>
+    <a href="https://historynewsnetwork.org/article/169795"><img src="powerofwords.jpg"/></a>
+    <p>I am constantly inspired by the message that Dr. Martin Luther King, Jr. and & Mahatma Gandhi portrayed throughout their lives.  The commitment to positive change via nonviolence remind us all how powerful our words are and how important continued communication is to not just our social system, but to everything that we do.  It is my lifelong passion to emulate these leaders to inspire positive change by practicing communication with mindfulness, kindness, open mindedness, and love. </p>
+    
+    By:  Shawna Summers
+    <br><br>
+    <a href="https://historynewsnetwork.org/article/169795">Image Resource </a>
+</body>
+
+
+
+</html>
